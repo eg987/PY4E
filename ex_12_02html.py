2d9f6a1a400f3afda856112f0943e35fc9be8d5d
eg987 <154833588+eg987@users.noreply.github.com>
2025-07-25 10:15:58 -0500

    Update ex_12_02html.py
    Added exercise description

--- ex_12_02html.py
+++ ex_12_02html.py
@@ -1,3 +1,32 @@
+'''
+Scraping Numbers from HTML using BeautifulSoup In this assignment you will write a Python program similar to http://www.py4e.com/code3/urllink2.py. The program will use urllib to read the HTML from the data files below, and parse the data, extracting numbers and compute the sum of the numbers in the file.
+
+The file is a table of names and comment counts. You can ignore most of the data in the file except for lines like the following:
+
+Example:
+<tr><td>Modu</td><td><span class="comments">90</span></td></tr>
+<tr><td>Kenzie</td><td><span class="comments">88</span></td></tr>
+<tr><td>Hubert</td><td><span class="comments">87</span></td></tr>
+
+You are to find all the <span> tags in the file and pull out the numbers from the tag and sum the numbers.
+
+Look at the sample code provided. It shows how to find all of a certain kind of tag, loop through the tags and extract the various aspects of the tags.
+
+Example:
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+   # Look at the parts of a tag
+   print 'TAG:',tag
+   print 'URL:',tag.get('href', None)
+   print 'Contents:',tag.contents[0]
+   print 'Attrs:',tag.attrs
+...   
+You need to adjust this code to look for span tags and pull out the text content of the span tag, convert them to integers and add them up to complete the assignment.
+
+Python for Everybody: Exploring Data Using Python 3
+by Charles R. Severance
+'''
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
