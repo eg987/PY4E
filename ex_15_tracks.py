4b8c232b09a5ac0d204771244f136a4f9562e8b7
eg987 <154833588+eg987@users.noreply.github.com>
2025-07-25 11:07:08 -0500

    Update ex_15_tracks.py
    Added exercise description

--- ex_15_tracks.py
+++ ex_15_tracks.py
@@ -1,4 +1,48 @@
-import sqlite3
+'''
+This application will read an iTunes export file in CSV format and produce a properly normalized database with this structure:
+
+CREATE TABLE Artist (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Genre (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Album (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    artist_id  INTEGER,
+    title   TEXT UNIQUE
+);
+
+CREATE TABLE Track (
+    id  INTEGER NOT NULL PRIMARY KEY 
+        AUTOINCREMENT UNIQUE,
+    title TEXT  UNIQUE,
+    album_id  INTEGER,
+    genre_id  INTEGER,
+    len INTEGER, rating INTEGER, count INTEGER
+);
+If you run the program multiple times in testing or with different files, make sure to empty out the data before each run.import sqlite3
+
+You can use this code as a starting point for your application: http://www.py4e.com/code3/tracks.zip. The ZIP file contains the tracks.csv file to be used for this assignment. You can export your own tracks from iTunes and create a database, but for the database that you turn in for this assignment, only use the tracks.csv data that is provided. You can adapt the tracks_csv.py application in the zip file to complete the assignment.
+
+To grade this assignment, the program will run a query like this on your uploaded database and look for the data it expects to see:
+
+SELECT Track.title, Artist.name, Album.title, Genre.name 
+    FROM Track JOIN Genre JOIN Album JOIN Artist 
+    ON Track.genre_id = Genre.ID and Track.album_id = Album.id 
+        AND Album.artist_id = Artist.id
+    ORDER BY Artist.name LIMIT 3
+The expected result of the modified query on your database is 3 entries with data in columns: 
+Track	Artist	Album	Genre
+
+Python for Everybody: Exploring Data Using Python 3
+by Charles R. Severance
+
+'''
 
 conn = sqlite3.connect('trackdb.sqlite')
 cur = conn.cursor()
