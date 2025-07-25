2518c56a1c641d7672a2193aff4276e709b2232c
eg987 <154833588+eg987@users.noreply.github.com>
2025-07-25 10:27:18 -0500

    Update ex_13_01xml.py
    Added exercise description

--- ex_13_01.py
+++ ex_13_01.py
@@ -1,3 +1,9 @@
+'''
+Extracting Data from XML
+
+In this assignment you will write a Python program somewhat similar to http://www.py4e.com/code3/xml3.py. The program will prompt for a URL, read the XML data from that URL using urllib and then parse and extract the comment counts from the XML data, compute the sum of the numbers in the file.
+'''
+
 import urllib.request, urllib.parse, urllib.error
 import xml.etree.ElementTree as ET
 import ssl
