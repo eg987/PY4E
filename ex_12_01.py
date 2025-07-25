0028a83161afc4a6ee2542ec9a42fa5ef613be03
eg987 <154833588+eg987@users.noreply.github.com>
2025-07-25 09:00:02 -0500

    Update ex_12_01.py
    Added exercise description

--- ex_12_01.py
+++ ex_12_01.py
@@ -1,3 +1,10 @@
+'''
+Exercise 1: Change the socket program socket1.py to prompt the user for the URL so it can read any web page. You can use split('/') to break the URL into its component parts so you can extract the host name for the socket connect call. Add error checking using try and except to handle the condition where the user enters an improperly formatted or non-existent URL.
+
+Python for Everybody: Exploring Data Using Python 3
+by Charles R. Severance
+'''
+
 import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
