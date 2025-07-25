0efa58d5ec1671713875c1b0e9556900a272ecb6
eg987 <154833588+eg987@users.noreply.github.com>
2025-07-25 08:48:41 -0500

    Update ex_10_02.py
    Added ex description

--- ex_10_02.py
+++ ex_10_02.py
@@ -1,3 +1,24 @@
+'''
+10.2 Write a program to read through the mbox-short.txt and figure out the distribution by hour of the day for each of the messages. You can pull the hour out from the 'From ' line by finding the time and then splitting the string a second time using a colon.
+
+#Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below.
+#Sample line: From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+Sample Execution:
+04 3
+06 1
+07 1
+09 2
+10 3
+11 6
+14 1
+15 2
+16 4
+17 2
+18 1
+19 1
+'''
+
+
 fname = input("Enter file name: ")
 if len(fname) < 1:
     fname = "mbox-short.txt"
