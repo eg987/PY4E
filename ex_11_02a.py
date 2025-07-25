dc82c3cac36ae060f8f5d7fe29d9818a6938be02
eg987 <154833588+eg987@users.noreply.github.com>
2025-07-25 08:57:41 -0500

    Update ex_11_02a.py
    Added exercise description

--- ex_11_02a.py
+++ ex_11_02a.py
@@ -1,3 +1,21 @@
+'''
+Exercise  11.2: Write a program to look for lines of the form
+
+'New Revision: 39771'
+
+and extract the number from each of the lines using a regular expression and
+the findall() method. Compute the average of the numbers and print out the
+average.
+
+Enter file:mbox.txt
+38549.7949721
+
+Enter file:mbox-short.txt
+39756.9259259
+
+Python for Everybody: Exploring Data Using Python 3
+by Charles R. Severance
+'''
 import re
 total=0
 count=0
