2caf181d1546fbf7f691ec563a69954be225cd73
eg987 <154833588+eg987@users.noreply.github.com>
2025-07-25 10:30:42 -0500

    Update ex_13_jsoncount.py
    Added exercise description

--- ex_13_jsoncount.py
+++ ex_13_jsoncount.py
@@ -1,3 +1,15 @@
+'''
+Extracting Data from JSON
+
+In this assignment you will write a Python program somewhat similar to 
+http://www.py4e.com/code3/json2.py. The program will prompt for a URL, read the JSON 
+data from that URL using urllib and then parse and extract the comment counts from the
+JSON data, compute the sum of the numbers in the file and enter the sum below:
+
+Python for Everybody: Exploring Data Using Python 3
+by Charles R. Severance
+'''
+
 import urllib.request, urllib.parse, urllib.error
 import json
 import xml.etree.ElementTree as ET
